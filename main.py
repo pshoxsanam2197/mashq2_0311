aecfb083b5d7e92289a8f002f4d473d1a7b16d54
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2025-11-03 18:06:57 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+#1
+kop = 1
+for i in range(1, 6):
+    kop *= i
+
+print(kop)
+
+#2
+kop = 1
+for i in range(1, 11):
+    if i % 2 == 0:
+       kop *= i
+
+print(kop)
+
+# 3
+n = int(input("son kirit: "))
+kop = 1
+for i in range(1, n+1):
+    kop *= i
+
+print(kop)
+
+
+#4
+kop = 1
+for i in range(3, 10):
+    kop *= i
+
+print(kop)
+
+# 5
+kop = 1
+for i in range(1, 8):
+    if i % 2 != 0:
+       kop *= i
+
+print(kop)
+
+
+
